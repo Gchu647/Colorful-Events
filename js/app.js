4a25aeb95a3041d87a983431023330b4a8ceb571
George <quicksilver-ga@hotmail.com>
2018-06-22 11:19:23 -1000

    Complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 
+var clickColor = document.getElementById("button");
+clickColor.addEventListener("click", randomColor);
+
+function randomColor() {
+    var color1 = Math.floor(Math.random() * 256);
+    var color2 = Math.floor(Math.random() * 256);
+    var color3 = Math.floor(Math.random() * 256);
+    document.body.style.backgroundColor = "rgb(" + color1 + "," + color2 + "," + color3 + ")";
+}
